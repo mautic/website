e20f83f93cec669b85e83e0680ebae30ba11a3d5
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-03-26 23:26:04 +0100

    add/edit marketplace item

--- components/AddNewExtension.php
+++ components/AddNewExtension.php
@@ -3,11 +3,19 @@
 use Cms\Classes\ComponentBase;
 use trka\Marketplace\Classes\GhRepoClient;
 use trka\Marketplace\Models\Downloads;
+use Auth;
 
 class AddNewExtension extends ComponentBase
 {
-    public $download;
     public $extensionRepo;
+
+    public function onRun()
+    {
+        $slug = $this->property('slug');
+        if ($slug) {
+            $this->page['download'] = Downloads::where('slug', $slug)->first();
+        }
+    }
 
     public function componentDetails()
     {
@@ -28,6 +36,7 @@ class AddNewExtension extends ComponentBase
         $validator = \Validator::make(
             $form = \Input::all(),
             [
+                'name' => 'required',
                 'type' => 'required',
                 'repository_url' => 'required',
             ]
@@ -36,6 +45,7 @@ class AddNewExtension extends ComponentBase
             throw new \ValidationException($validator);
         }
 
+        $downloadId = (int)$form['downloadid'];
         //-- set remote repo fields
         $repoUrl = $form['repository_url'];
         $repoProvider = '';
@@ -46,11 +56,24 @@ class AddNewExtension extends ComponentBase
         }
 
         $user = \Auth::getUser();
+        $model = Downloads::where('id', $downloadId)->first();
 
-        $model = new Downloads();
+        if ($model) {
+            $canEdit = $model->userCanEdit();
+            if (!$canEdit) {
+                // @todo: should return unauthorized.
+                \App::abort(403, 'Unauthorized.');
+                return false;
+            }
+        }else {
+
+            $model = new Downloads();
+            $model->review_status = 'pending';
+        }
+
         $model->name = $form['name'];
         $model->description = $form['description'];
-        $model->review_status = 'pending';
+
         $model->repository_provider = $repoProvider;
         $model->repository_url = $repoUrl;
         $model->user = $user;
@@ -58,15 +81,20 @@ class AddNewExtension extends ComponentBase
         //-- if there are empty props, use remote repo to fill them in.
         if ($model->name === '' || $model->description === '') {
             $hydrated = $this->hydrateFromRemote($repoProvider, $repoUrl);
-            if ($model->name === '') {
-                $model->name = $hydrated->name();
-            }
-            if ($model->description === '') {
-                $model->description = $hydrated->shortDescription();
-            }
+
+                if ($model->name === '' && method_exists($model, 'name')) {
+                    $model->name = $hydrated->name();
+                }
+                if ($model->description === '' && method_exists($model, 'shortDescription')) {
+                    $model->description = $hydrated->shortDescription();
+                }
         }
 
         $model->save();
+
+        $me                  = Auth::getUser();
+        return \Redirect::to('mauticians/'.$me->username);
+
 
         // @todo return appropriate response.
         return [
